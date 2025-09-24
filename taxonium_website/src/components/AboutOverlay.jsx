07a8d62b63dcf5afb4ad4969e02199d699427c8b
Emily Fields <80897288+emily-fields@users.noreply.github.com>
2025-09-24 16:30:17 -0700

    Remove from website

--- taxonium_website/src/components/AboutOverlay.jsx
+++ taxonium_website/src/components/AboutOverlay.jsx
@@ -1,5 +1,5 @@
 import React, { useRef, useEffect } from "react";
-import TaxoniumModal from "./TaxoniumModal";
+import Modal from "react-modal";
 
 const modalStyle = {
   content: {
@@ -24,7 +24,7 @@ const modalStyle = {
 
 function AboutOverlay({ enabled, setEnabled, overlayContent }) {
   return (
-    <TaxoniumModal
+    <Modal
       isOpen={enabled}
       onRequestClose={() => setEnabled(false)}
       style={modalStyle}
@@ -76,7 +76,7 @@ function AboutOverlay({ enabled, setEnabled, overlayContent }) {
           </p>
         </div>
       )}
-    </TaxoniumModal>
+    </Modal>
   );
 }
 
